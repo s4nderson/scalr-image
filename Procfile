b3acaf859621b9659e422f471391464443245861
david.sanderson <david.sanderson@team.pottermore.com>
2016-10-04 21:34:35 +0100

    @s4nderson #SPIKE headless image scaling

---
+++ Procfile
@@ -0,0 +1,1 @@
+scalr: java -cp build/libs/scalr-image-all.jar pm.ImageScalr
---
+++ src/main/groovy/pm/ImageScalr.groovy
@@ -0,0 +1,8 @@
+package pm
+
+class ImageScalr {
+
+    static void main(String[] args) {
+        println 'you got it'
+    }
+}

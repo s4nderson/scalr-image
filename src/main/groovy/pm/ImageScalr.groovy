ab4c13cf657a30e044dd6e4de020daad559542f5
david.sanderson <david.sanderson@team.pottermore.com>
2016-10-04 22:11:20 +0100

    @s4nderson add the scalr

--- src/main/groovy/pm/ImageScalr.groovy
+++ src/main/groovy/pm/ImageScalr.groovy
@@ -1,8 +1,15 @@
 package pm
 
+import org.imgscalr.Scalr
+
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
 class ImageScalr {
 
     static void main(String[] args) {
+        BufferedImage srcImage = ImageIO.read(this.classLoader.getResourceAsStream('/gryffindor_texture_enormous.jpg'))
+        BufferedImage scaledImage = Scalr.resize(srcImage, 150)
         println 'you got it'
     }
 }
